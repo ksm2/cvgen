fe91a50e6908093daeabf98ab2372c20cf14cbd9
Konstantin Simon Maria Möllers <ksm.moellers@gmail.com>
2022-03-02 22:07:06 +0100

    Refactor formatDate

--- src/__tests__/utils.test.ts
+++ src/__tests__/utils.test.ts
@@ -1,4 +1,4 @@
-import { capitalize, formatDate, formatDateSpan } from '../utils';
+import { capitalize, formatDate, formatDateSpan, formatYearMonth } from '../utils';
 
 describe('utils', () => {
   test('formatDateSpan', () => {
@@ -7,12 +7,25 @@ describe('utils', () => {
   });
 
   test('formatDate', () => {
+    expect(formatDate('en-GB', '')).toBe('');
+    expect(formatDate('en-GB', 'today')).toBe('today');
     expect(formatDate('en-GB', '2020/01')).toBe('Jan 2020');
     expect(formatDate('en-GB', '2021/03')).toBe('Mar 2021');
     expect(formatDate('en-GB', '2022/09')).toBe('Sept 2022');
+    expect(formatDate('en-GB', '2022')).toBe('2022');
+    expect(() => formatDate('en-GB', 'foo')).toThrow(TypeError);
+    expect(formatDate('de-DE', '')).toBe('');
+    expect(formatDate('de-DE', 'today')).toBe('heute');
     expect(formatDate('de-DE', '2020/01')).toBe('Jan. 2020');
     expect(formatDate('de-DE', '2021/03')).toBe('März 2021');
     expect(formatDate('de-DE', '2022/09')).toBe('Sept. 2022');
+    expect(formatDate('de-DE', '2022')).toBe('2022');
+    expect(() => formatDate('de-DE', 'foo')).toThrow(TypeError);
+  });
+
+  test('formatYearMonth', () => {
+    expect(formatYearMonth('en-GB', 2020, 7)).toBe('Jul 2020');
+    expect(formatYearMonth('de-DE', 2020, 7)).toBe('Juli 2020');
   });
 
   test('capitalize', () => {
--- src/utils.ts
+++ src/utils.ts
@@ -5,24 +5,18 @@ export function formatDateSpan(lang: string, text: string) {
 }
 
 export function formatDate(lang: string, text: string): string {
-  if (!text) {
-    return '';
-  }
+  return match([
+    [empty(), () => ''],
+    [equals('today'), (text) => translate(lang, text)],
+    [regexp(/^(\d{4})\/(\d{2})$/), (year, month) => formatYearMonth(lang, +year, +month)],
+    [regexp(/^(\d{4})$/), (year) => year],
+    throws(new TypeError(`Cannot format date "${text}"`)),
+  ])(text);
+}
 
-  if (text === 'today') {
-    return translate(lang, text);
-  }
-
-  if (text.match(/(\d{4})\/(\d{2})/)) {
-    const formatter = new Intl.DateTimeFormat(lang, { month: 'short', year: 'numeric' });
-    return formatter.format(new Date(+RegExp.$1, +RegExp.$2 - 1, 1, 0, 0, 0, 0));
-  }
-
-  if (text.match(/(\d{4})/)) {
-    return RegExp.$1;
-  }
-
-  throw new TypeError(`Cannot format date "${text}"`);
+export function formatYearMonth(lang: string, year: number, month: number): string {
+  const formatter = new Intl.DateTimeFormat(lang, { month: 'short', year: 'numeric' });
+  return formatter.format(new Date(year, month - 1, 1, 0, 0, 0, 0));
 }
 
 export function translate(lang: string, text: string): string {
@@ -38,3 +32,46 @@ export function labelPhone(number: string) {
   const { groups = {} } = /^(?<country>\+\d+)-(?<code>\d+)-(?<number>\d+)$/.exec(number)!;
   return `${groups.country} (0)${groups.code} ${groups.number}`;
 }
+
+function match<T>(cases: [...Case<T, any>[], CaseValue<T, []>]): (value: string) => T {
+  return function matchExec(text: string): T {
+    const cs = cases.slice(0, -1) as Case<T, any>[];
+    for (const [cond, value] of cs) {
+      const match = cond(text);
+      if (match !== null) {
+        return value(...match);
+      }
+    }
+    const fallback = cases[cases.length - 1] as CaseValue<T, []>;
+    return fallback();
+  };
+}
+
+function empty(): CaseCondition<[]> {
+  return function emptyCase(text) {
+    return text ? null : [];
+  };
+}
+
+function equals(str: string): CaseCondition<[string]> {
+  return function equalsCase(text) {
+    return text === str ? [text] : null;
+  };
+}
+
+function regexp(re: RegExp): CaseCondition<string[]> {
+  return function regexpCase(text) {
+    const match = re.exec(text);
+    return match ? match.slice(1) : null;
+  };
+}
+
+function throws(error: Error): CaseValue<any, any> {
+  return function throwsCase() {
+    throw error;
+  };
+}
+
+type Case<T, U extends any[]> = [CaseCondition<U>, CaseValue<T, U>];
+type CaseCondition<U> = (text: string) => U | null;
+type CaseValue<T, U extends any[]> = (...matches: U) => T;
