540f2bec5c4cdf6b8dc4f851fe1b13367c1ebc08
Konstantin Simon Maria Möllers <ksm.moellers@gmail.com>
2022-03-02 20:36:41 +0100

    Show help dialog

--- scripts/build.ts
+++ scripts/build.ts
@@ -3,6 +3,11 @@ import path from 'path'
 import { readFile, renderPDF } from '../src'
 
 async function build() {
+  if (process.argv.includes('--help') || process.argv.includes('-h') || process.argv.length < 4) {
+    help();
+    return;
+  }
+
   const filename = path.resolve(process.cwd(), process.argv[2])
   const output = path.resolve(process.cwd(), process.argv[3])
   const document = await readFile(filename)
@@ -13,6 +18,18 @@ async function build() {
   }
   const stream = fs.createWriteStream(output)
   renderPDF(document, stream)
+}
+
+function help() {
+  process.stdout.write(`\
+Generator for résumés
+
+USAGE
+  ${process.argv[1]} INPUT OUTPUT
+
+OPTIONS
+  -h, --help  Show this help
+`)
 }
 
 build().catch(console.error)
